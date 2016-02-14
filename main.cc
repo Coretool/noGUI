b59ea3545b2495c2d817c0481f43165797a0eb5f
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2016-02-14 08:36:12 +0100

    Clean-up & options to don't use system libraries

---
+++ main.cc
@@ -0,0 +1,26 @@
+#include <QApplication>
+#include <QWebView>
+#include <QtPlugin>
+
+
+/*  TODO
+ *  add WebView markup (eg. link markup)(?)
+ *  add ability to change webview content while app is running
+ *  check if the URL is correct (-> if-else)
+ */
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+
+  QWebView view;
+
+  if(QCoreApplication::arguments().count() < 2)
+    perror("You have to add url like http://example.com");
+  else
+  {
+    view.show(); //shows full screen on fb and EGLFS
+    view.load(QUrl(QCoreApplication::arguments().at(1)));
+
+    return app.exec();
+  }
+}
